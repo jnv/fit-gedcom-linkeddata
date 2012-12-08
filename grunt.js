5af90107085425898ee81316b5675fed9b8b7d04
Jan Vlnas <git@jan.vlnas.cz>
2012-12-08 22:37:57 +0100

    Prepare dev dependencies

---
+++ grunt.js
@@ -0,0 +1,26 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    server: {
+      port: 8000
+    },
+    reload: {
+        //port: 6001,
+        proxy: {
+            host: 'localhost',
+            port: '<config:server.port>' // should match server.port config
+        }
+    },
+    watch: {
+      files: ['index.html'],
+      tasks: 'lint reload'
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-reload');
+  // Default task.
+  grunt.registerTask('default', 'server reload watch');
+
+};
